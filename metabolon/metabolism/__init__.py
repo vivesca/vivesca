33c820d33a3cb167b28f8a5b48e08741f4c2a667
Terry Li <terry@terryli.dev>
2026-03-31 15:53:34 +0000

    golem: daemon auto-commit

--- metabolon/metabolism/__init__.py
+++ metabolon/metabolism/__init__.py
@@ -4,26 +4,10 @@ One mechanism, any substrate. The SUBSTRATES registry maps target names
 to Substrate implementations. New targets register by adding to it.
 """
 
-from metabolon.metabolism.fitness import Emotion, sense_affect
-from metabolon.metabolism.gates import GateResult, reflex_check
-from metabolon.metabolism.repair import ImmuneRequest, immune_response
-from metabolon.metabolism.signals import SensorySystem, Stimulus
-from metabolon.metabolism.substrate import Substrate
 from metabolon.metabolism.substrates import receptor_catalog
-from metabolon.metabolism.variants import Genome
 
 SUBSTRATES = receptor_catalog
 
 __all__ = [
     "SUBSTRATES",
-    "Emotion",
-    "GateResult",
-    "Genome",
-    "ImmuneRequest",
-    "SensorySystem",
-    "Stimulus",
-    "Substrate",
-    "immune_response",
-    "reflex_check",
-    "sense_affect",
 ]
